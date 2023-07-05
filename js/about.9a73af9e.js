d22fcac1713aa4f8254d8105c939934437184ebf
Dinesh Kumar <Prajwal>
2023-07-06 02:33:57 +0530

    adding dist files

---
+++ js/about.9a73af9e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcompound_finance_ui_clone"]=self["webpackChunkcompound_finance_ui_clone"]||[]).push([[443],{6589:function(n,u,c){c.r(u),c.d(u,{default:function(){return l}});var e=c(3396);const a={class:"about"},o=(0,e._)("h1",null,"This is an about page",-1),i=[o];function t(n,u){return(0,e.wg)(),(0,e.iD)("div",a,i)}var r=c(89);const s={},f=(0,r.Z)(s,[["render",t]]);var l=f}}]);
+//# sourceMappingURL=about.9a73af9e.js.map
